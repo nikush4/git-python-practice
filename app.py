1c342fb6bd8ccdfd2e2ec222d939383ffd860ab4
nikol <nikolnovikov99@gmail.com>
2026-07-13 13:37:55 +0300

    Revert "Change greeting wording on main"
    This reverts commit 478ab7ab6c04bca11042ef0eda7df3095af86994.

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from datetime import datetime
 def greet(name):
     current_time = datetime.now().strftime("%H:%M:%S")
     return f"Hey there, {name}! Current time: {current_time}"
-
-
 if __name__ == "__main__":
     user = "World"
     print(greet(user))
 
 
 # small update
+
