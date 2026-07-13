b8dfffd0483a74068853da4534a724caf121a9a9
nikol <nikolnovikov99@gmail.com>
2026-07-13 13:16:30 +0300

    Change greeting wording on feature branch

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def greet(name):
     current_time = datetime.now().strftime("%H:%M:%S")
-    return f"Hello, {name}! The time is {current_time}."
+   return f"Greetings, {name}! Current time: {current_time}"
 
 
 if __name__ == "__main__":
